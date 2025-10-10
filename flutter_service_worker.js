0e4c601f1d460b645ef5a7fe299e96cccfc65f01
idplaystore49-design <idplaystore49@gmail.com>
2025-10-10 12:34:07 +0500

    Built web
    Branch: main
    Commit: 62e61b39cc0db72afd932d94673e14e1d0059f1b
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "803868df059f9b01a2a6616d899a47bd
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "3ed9b33030a9b8ac9f568dbf23f6ccfc",
+"flutter_bootstrap.js": "76b7c0e1ea387e6a4d027e25fc57313e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
